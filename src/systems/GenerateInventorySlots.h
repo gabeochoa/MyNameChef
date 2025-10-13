4b9a69fdede1a3547f9adde1f37700875a19d949
Gabe Ochoa <gabeochoa@meta.com>
2025-10-12 20:54:37 -0400

    bf - fix shop slots disappearing after battle

--- src/systems/GenerateInventorySlots.h
+++ src/systems/GenerateInventorySlots.h
@@ -1,72 +1,60 @@
 #pragma once
 
-#include "../components/has_tooltip.h"
-#include "../components/is_dish.h"
-#include "../components/is_inventory_item.h"
-#include "../components/render_order.h"
-#include "../components/transform.h"
-#include "../dish_types.h"
-#include "../render_constants.h"
+#include "../components/is_drop_slot.h"
+#include "../game_state_manager.h"
 #include "../shop.h"
-#include "../tooltip.h"
 #include <afterhours/ah.h>
-#include <afterhours/src/plugins/color.h>
-#include <afterhours/src/plugins/texture_manager.h>
-#include <magic_enum/magic_enum.hpp>
 
 using namespace afterhours;
 
 struct GenerateInventorySlots : System<> {
-  bool initialized = false;
+  GameStateManager::Screen last_screen = GameStateManager::Screen::Main;
 
-  void once(float) override {
-    if (initialized)
-      return;
-    initialized = true;
-
-    for (int i = 0; i < INVENTORY_SLOTS; ++i) {
-      auto position = calculate_inventory_position(i);
-      make_drop_slot(INVENTORY_SLOT_OFFSET + i, position,
-                     vec2{SLOT_SIZE, SLOT_SIZE}, true, true);
-    }
-
-    // Create a sell drop slot to the right of inventory
-    {
-      vec2 pos{static_cast<float>(SELL_SLOT_X),
-               static_cast<float>(SELL_SLOT_Y)};
-      make_drop_slot(SELL_SLOT_ID, pos, vec2{SLOT_SIZE, SLOT_SIZE}, true,
-                     false);
-    }
-
-    add_starter_inventory_item();
-    EntityHelper::merge_entity_arrays();
+  virtual bool should_run(float) override {
+    auto &gsm = GameStateManager::get();
+    return gsm.is_game_active();
   }
 
-private:
-  void add_starter_inventory_item() {
-    auto &entity = EntityHelper::createEntity();
-    auto position = calculate_inventory_position(0);
-    auto dish_type = get_random_dish();
-    auto dish_info = get_dish_info(dish_type);
+  void once(float) override {
+    auto &gsm = GameStateManager::get();
 
-    entity.addComponent<Transform>(position, vec2{SLOT_SIZE, SLOT_SIZE});
-    entity.addComponent<IsDish>(dish_type);
-    entity.addComponent<IsInventoryItem>();
-    entity.addComponent<HasRenderOrder>(RenderOrder::InventoryItems,
-                                        RenderScreen::Shop);
-    // Attach sprite using dish atlas grid indices
-    {
-      const auto frame = afterhours::texture_manager::idx_to_sprite_frame(
-          dish_info.sprite.i, dish_info.sprite.j);
-      entity.addComponent<afterhours::texture_manager::HasSprite>(
-          position, vec2{SLOT_SIZE, SLOT_SIZE}, 0.f, frame,
-          render_constants::kDishSpriteScale,
-          raylib::Color{255, 255, 255, 255});
+    // Generate inventory slots when entering shop screen
+    if (gsm.active_screen == GameStateManager::Screen::Shop &&
+        last_screen != GameStateManager::Screen::Shop) {
+
+      // Check if inventory slots already exist
+      bool inventory_slots_exist = false;
+      for (auto &ref : afterhours::EntityQuery()
+                           .template whereHasComponent<IsDropSlot>()
+                           .gen()) {
+        const auto &drop_slot = ref.get().template get<IsDropSlot>();
+        if (drop_slot.accepts_inventory_items && drop_slot.accepts_shop_items) {
+          inventory_slots_exist = true;
+          break;
+        }
+      }
+
+      // Only create inventory slots if they don't exist
+      if (!inventory_slots_exist) {
+        for (int i = 0; i < INVENTORY_SLOTS; ++i) {
+          auto position = calculate_inventory_position(i);
+          make_drop_slot(INVENTORY_SLOT_OFFSET + i, position,
+                         vec2{SLOT_SIZE, SLOT_SIZE}, true, true);
+        }
+
+        // Create a sell drop slot to the right of inventory
+        {
+          vec2 pos{static_cast<float>(SELL_SLOT_X),
+                   static_cast<float>(SELL_SLOT_Y)};
+          make_drop_slot(SELL_SLOT_ID, pos, vec2{SLOT_SIZE, SLOT_SIZE}, true,
+                         false);
+        }
+
+        EntityHelper::merge_entity_arrays();
+        log_info("Generated inventory slots for shop screen");
+      }
     }
-    entity.addComponent<HasTooltip>(generate_dish_tooltip(dish_type));
-    entity.get<IsInventoryItem>().slot = INVENTORY_SLOT_OFFSET;
 
-    log_info("Added starter dish: {} to inventory",
-             magic_enum::enum_name(dish_type));
+    last_screen = gsm.active_screen;
   }
 };
--- src/systems/GenerateShopSlots.h
+++ src/systems/GenerateShopSlots.h
@@ -1,26 +1,51 @@
 #pragma once
 
 #include "../components/is_drop_slot.h"
-#include "../components/render_order.h"
-#include "../components/transform.h"
+#include "../game_state_manager.h"
 #include "../shop.h"
 #include <afterhours/ah.h>
 
 using namespace afterhours;
 
 struct GenerateShopSlots : System<> {
-  bool initialized = false;
+  GameStateManager::Screen last_screen = GameStateManager::Screen::Main;
+
+  virtual bool should_run(float) override {
+    auto &gsm = GameStateManager::get();
+    return gsm.is_game_active();
+  }
 
   void once(float) override {
-    if (initialized)
-      return;
-    initialized = true;
+    auto &gsm = GameStateManager::get();
 
-    for (int i = 0; i < SHOP_SLOTS; ++i) {
-      auto position = calculate_slot_position(i, SHOP_START_X, SHOP_START_Y);
-      make_drop_slot(i, position, vec2{SLOT_SIZE, SLOT_SIZE}, false, true);
+    // Generate shop slots when entering shop screen
+    if (gsm.active_screen == GameStateManager::Screen::Shop &&
+        last_screen != GameStateManager::Screen::Shop) {
+
+      // Check if shop slots already exist
+      bool shop_slots_exist = false;
+      for (auto &ref : afterhours::EntityQuery()
+                           .template whereHasComponent<IsDropSlot>()
+                           .gen()) {
+        const auto &drop_slot = ref.get().template get<IsDropSlot>();
+        if (!drop_slot.accepts_inventory_items && drop_slot.accepts_shop_items) {
+          shop_slots_exist = true;
+          break;
+        }
+      }
+
+      // Only create shop slots if they don't exist
+      if (!shop_slots_exist) {
+        for (int i = 0; i < SHOP_SLOTS; ++i) {
+          auto position = calculate_slot_position(i, SHOP_START_X, SHOP_START_Y);
+          make_drop_slot(i, position, vec2{SLOT_SIZE, SLOT_SIZE}, false, true);
+        }
+
+        EntityHelper::merge_entity_arrays();
+        log_info("Generated shop slots for shop screen");
+      }
     }
 
-    EntityHelper::merge_entity_arrays();
+    last_screen = gsm.active_screen;
   }
 };
