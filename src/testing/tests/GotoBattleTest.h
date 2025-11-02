a30b8d87ac2d4cf36d615eb0131378e2a238b65c
Gabe Ochoa <gabeochoa@meta.com>
2025-11-01 22:09:19 -0400

    migrate goto_battle to new test framework

--- src/testing/tests/GotoBattleTest.h
+++ src/testing/tests/GotoBattleTest.h
@@ -1,11 +1,29 @@
 #pragma once
 
 #include "../../game_state_manager.h"
-#include "../TestInteraction.h"
+#include "../test_macros.h"
 
-struct GotoBattleTest {
-  static void execute() {
-    TestInteraction::set_screen(GameStateManager::Screen::Battle);
-    GameStateManager::get().update_screen();
+TEST(goto_battle) {
+  // Check if we're already on Battle screen (from a previous test iteration
+  // after click)
+  GameStateManager::get().update_screen();
+  auto &gsm = GameStateManager::get();
+  if (gsm.active_screen == GameStateManager::Screen::Battle) {
+    app.wait_for_ui_exists("Skip to Results", 5.0f);
+    return; // Test is done!
   }
-};
+
+  app.launch_game();
+  app.wait_for_ui_exists("Play");
+  app.click("Play");
+  app.wait_for_screen(GameStateManager::Screen::Shop, 10.0f);
+  app.wait_for_ui_exists("Next Round");
+
+  // TODO: Add dish creation back once inventory setup changes
+  // Currently relying on existing inventory from game setup for export menu
+  // snapshot
+
+  app.click("Next Round");
+  app.wait_for_screen(GameStateManager::Screen::Battle, 15.0f);
+  app.wait_for_ui_exists("Skip to Results", 5.0f);
+}
