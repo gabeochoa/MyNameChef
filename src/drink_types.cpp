75c4d6732201e554c2ee0ac039a13e17f016b3ee
Gabe Ochoa <gabeochoa@meta.com>
2025-11-16 22:35:39 -0500

    implement get_drink_info for tier 2 drinks

--- src/drink_types.cpp
+++ src/drink_types.cpp
@@ -11,6 +11,14 @@ DrinkInfo get_drink_info(DrinkType type) {
     return DrinkInfo{"Coffee", 2, SpriteLocation{0, 5}};
   case DrinkType::RedSoda:
     return DrinkInfo{"Red Soda", 2, SpriteLocation{5, 5}};
+  case DrinkType::HotCocoa:
+    return DrinkInfo{"Hot Cocoa", 2, SpriteLocation{1, 5}};
+  case DrinkType::RedWine:
+    return DrinkInfo{"Red Wine", 3, SpriteLocation{10, 5}};
+  case DrinkType::GreenSoda:
+    return DrinkInfo{"Green Soda", 2, SpriteLocation{4, 5}};
+  case DrinkType::BlueSoda:
+    return DrinkInfo{"Blue Soda", 2, SpriteLocation{3, 5}};
   default:
     log_error("get_drink_info: Unhandled drink type");
     return DrinkInfo{"Water", 1, SpriteLocation{7, 5}};
