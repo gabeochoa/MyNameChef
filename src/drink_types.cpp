de5406333a9937c54c500e3c168128d1ba3e7dad
Gabe Ochoa <gabeochoa@meta.com>
2025-11-16 22:35:21 -0500

    implement get_drink_info for tier 1 drinks

--- src/drink_types.cpp
+++ src/drink_types.cpp
@@ -1,11 +1,20 @@
 #include "drink_types.h"
+#include "log.h"
 
 DrinkInfo get_drink_info(DrinkType type) {
   switch (type) {
   case DrinkType::Water:
     return DrinkInfo{"Water", 1, SpriteLocation{7, 5}};
+  case DrinkType::OrangeJuice:
+    return DrinkInfo{"Orange Juice", 1, SpriteLocation{2, 5}};
+  case DrinkType::Coffee:
+    return DrinkInfo{"Coffee", 2, SpriteLocation{0, 5}};
+  case DrinkType::RedSoda:
+    return DrinkInfo{"Red Soda", 2, SpriteLocation{5, 5}};
+  default:
+    log_error("get_drink_info: Unhandled drink type");
+    return DrinkInfo{"Water", 1, SpriteLocation{7, 5}};
   }
-  return DrinkInfo{"Water", 1, SpriteLocation{7, 5}};
 }
 
 DrinkType get_random_drink() {
